f726061ac3ca801eeb376ca4f30f19f219bc099d
Scott McArthur <scottmcarthur@Scotts-iMac.local>
2017-02-11 18:04:22 +0000

    Initial Commit

---
+++ backup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+__author__ = 'element220'
+import sys
+import datetime
+import os
+import boto3
+
+if len(sys.argv) != 3:
+  quit()
+
+source_path = str(sys.argv[1])
+source_size = os.stat(source_path).st_size
+doy = datetime.datetime.now().timetuple().tm_yday
+bucket = str(sys.argv[2])
+name = str(sys.argv[3])
+key = "db/%s/%s.sql.gz" % (name,doy)
+
+print ("File: %s (%s bytes)" % (source_path, source_size))
+print ("DOY: %s" % doy)
+print ("Bucket: %s" % doy)
+print ("Key: %s" % key)
+
+s3_client = boto3.client("s3")
+s3_client.upload_file(source_path, bucket, key)
+print ("Completed")
